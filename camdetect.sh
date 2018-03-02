cedaf7044a7d706d956b84083abdd2fdc1cd4912
alkari <al_kari@manceps.com>
2018-03-02 10:07:02 -0800

    Revert "corrected camdetect.sh"
    This reverts commit 55246fb900ec291bf0f82a04083a0729c21ef64f.

--- camdetect.sh
+++ camdetect.sh
@@ -2,8 +2,8 @@
 
 xhost +
 
-docker run -it --rm  --privileged -\
-  -env DISPLAY=$DISPLAY \
+docker run -it --rm  --privileged \
+  --env DISPLAY=$DISPLAY \
   -v /tmp/.X11-unix:/tmp/.X11-unix:ro \
   -v /tmp:/workspace/camdetect/output \
   alkari/camdetect python3 camdetect/camdetect.py
